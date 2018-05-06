6407523e2a0495f84e9bd830ce874b95b77c1e9a
nekocode <nekocode.cn@gmail.com>
2018-05-07 01:36:22 +0800

    New commit

--- project-gallery/data/ui_data.js
+++
@@ -1,6 +0,0 @@
-var UI_DATA = {
-    "title": "Nekocode Open Source",
-    "github": "https://github.com/nekocode",
-    "description": "I'm a full-stack engineer and specialize in android development. I like open source. Here are some projects I have contributed to the community.",
-    "footer": "© Mark Yueng 2018 | <a href=\"https://github.com/nekocode/project-gallery\" target=\"_blank\">Source Code<\a>"
-}
